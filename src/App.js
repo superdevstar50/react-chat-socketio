c5b5311064778faac61ee46102ef3c377af9f1e9
superdevstar50 <superdevstar50@gmail.com>
2023-09-15 23:38:58 +0300

    add First Screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function App() {
-  return <div className="container p-2">Hello World</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-between p-5 w-50">
+        <a href="/create-link">
+          <button className="btn btn-primary"> Create Link </button>
+        </a>
+        <a href="/allopenedchats">
+          <button className="btn btn-primary"> All Opened Chats </button>
+        </a>
+        <a href="/closedchats">
+          <button className="btn btn-primary"> Closed Chats </button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
