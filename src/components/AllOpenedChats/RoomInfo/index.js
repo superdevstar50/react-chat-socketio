df091fe2066b1c842b122fe33274100645aac91f
superdevstar50 <superdevstar50@gmail.com>
2023-09-15 20:21:40 +0300

    changed some styling of RoomInfo

--- src/components/AllOpenedChats/RoomInfo/index.js
+++ src/components/AllOpenedChats/RoomInfo/index.js
@@ -5,13 +5,16 @@ function RoomInfo({ info }) {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">Name : {info.name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">
+        <h6 className="card-subtitle mb-2 text-muted border-bottom">
           Link : <button className="btn btn-link">{info.id}</button>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Time Left : {parseInt(
             info.time / 60
           )}m {info.time % 60}s{" "}
         </h6>
-        <p className="card-text">
+        <p
+          className="card-text overflow-y-scroll"
+          style={{ maxHeight: "300px" }}
+        >
           {info.history.map((item) => (
             <p>
               {item.userId} : {item.msg}
